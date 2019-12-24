c8ceb3d0e4baa98240793dc821c7829ee2bbb2df
kejun <527343714@qq.com>
2019-12-24 21:41:01 +0800

    ''

--- routes/blog.js
+++ routes/blog.js
@@ -8,16 +8,15 @@ const {
     delBlog
 } = require('../controller/blog')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')
 
 router.get('/list', (req, res, next) => {
     let author = req.query.author || ''
     const keyword = req.query.keyword || ''
 
-    /*if (req.query.isadmin) {
-        console.log('is admin')
+    if (req.query.isadmin) {
         // 管理员界面
         if (req.session.username == null) {
-            console.error('is admin, but no login')
             // 未登录
             res.json(
                 new ErrorModel('未登录')
@@ -26,7 +25,7 @@ router.get('/list', (req, res, next) => {
         }
         // 强制查询自己的博客
         author = req.session.username
-    }*/
+    }
 
     const result = getList(author, keyword)
     return result.then(listData => {
@@ -36,13 +35,13 @@ router.get('/list', (req, res, next) => {
     })
 });
 
-/*router.get('/detail', (req, res, next) => {
+router.get('/detail', (req, res, next) => {
     const result = getDetail(req.query.id)
     return result.then(data => {
         res.json(
             new SuccessModel(data)
         )
     })
-});*/
+});
 
 module.exports = router;
